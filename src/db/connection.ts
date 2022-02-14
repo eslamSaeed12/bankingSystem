6d10c54dbd1048be2a4ef4f869df463e176e2b1d
eslamSaeed12 <islam.s.mhmd1998@gmail.com>
2022-02-14 05:29:00 +0200

    fixing options in connections

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,17 @@
 import { createConnection } from 'typeorm';
 import { env } from '../utils/env';
+import { join } from 'path'
 
-const isDev = <string>env('NODE_ENV') === 'development' ? 'default' : 'stage';
+const default_ = env('NODE_ENV') === 'development';
 
-export const connection = createConnection(isDev);
+export const connection = default_ ? createConnection('default') : createConnection({
+    type: 'postgres',
+    database: env('TYPEORM_DATABASE'),
+    host: env('TYPEORM_HOST'),
+    migrations: [join(__dirname, 'migrations', '*.ts'), join(__dirname, 'migrations', '*.js')],
+    entities: [join(__dirname, 'models', '*.ts'), join(__dirname, 'models', '*.js')],
+    username: env('TYPEORM_USERNAME'),
+    password: env('TYPEORM_PASSWORD'),
+    synchronize: false,
+    logging: false
+});
